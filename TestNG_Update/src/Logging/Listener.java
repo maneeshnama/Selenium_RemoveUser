2c0952a54963dc41c0cbb047c903606673383146
Maneesh <maneesh@cne-global.com>
2017-07-06 12:40:56 -0500

    first commit

--- TestNG_Update/src/Logging/Listener.java
+++
@@ -1,5 +0,0 @@
-package Logging;
-
-public class Listener {
-
-}
--- TestNG_Update/testNGBatchFile.bat
+++
@@ -1,5 +0,0 @@
-set projectLocation=C:\Users\Maneesh\workspace\CNet_Global_Updated
-cd %projectLocation%
-set classpath=%projectLocation%\bin;%projectLocation%\lib\*
-java org.testng.TestNG %projectLocation%\TestNg.xml
-pause
