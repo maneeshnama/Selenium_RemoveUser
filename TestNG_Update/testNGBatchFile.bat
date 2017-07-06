17f83f6d1f36f648f5f391676bdc8f72c569426c
Maneesh <maneesh@cne-global.com>
2017-07-06 12:31:56 -0500

    first commit

---
+++ TestNG_Update/testNGBatchFile.bat
@@ -0,0 +1,5 @@
+set projectLocation=C:\Users\Maneesh\workspace\CNet_Global_Updated
+cd %projectLocation%
+set classpath=%projectLocation%\bin;%projectLocation%\lib\*
+java org.testng.TestNG %projectLocation%\TestNg.xml
+pause
