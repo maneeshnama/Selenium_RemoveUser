8cd3ab1ef848f0394bbef83ab8f52614f723418f
Maneesh <maneesh@cne-global.com>
2017-07-12 23:09:36 -0500

    second commit

--- ATUReports/Results/barChart.js
+++ ATUReports/Results/barChart.js
@@ -1,9 +1,9 @@
             $(document).ready(function(){
-                var s1 = [3,7,2,5,3,6,3,2,2,3];
-                var s2 = [1,0,1,1,0,0,0,1,1,0];
-                var s3 = [0,0,0,0,0,0,0,0,0,0];
+                var s1 = [2,2,2,2];
+                var s2 = [1,0,1,4];
+                var s3 = [0,0,0,0];
 
-var ticks = [11,12,13,14,15,16,17,18,19,20];    $.jqplot('bar', [s1, s2, s3], {
+var ticks = [1,2,3,4];    $.jqplot('bar', [s1, s2, s3], {
         animate: true,axesDefaults:{min:0,tickInterval: 1},        seriesColors: ["#7BB661", "#E03C31", "#21ABCD"],
         stackSeries: false,
         seriesDefaults: {
--- ATUReports/Results/lineChart.js
+++ ATUReports/Results/lineChart.js
@@ -1,9 +1,9 @@
             $(document).ready(function(){
-                var line1 = [2,4,2,2,3,4,3,4,6,4,3,7,2,5,3,6,3,2,2,3];
-                var line2 = [1,0,1,1,1,0,1,4,0,3,1,0,1,1,0,0,0,1,1,0];
-                var line3 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+                var line1 = [2,2,2,2];
+                var line2 = [1,0,1,4];
+                var line3 = [0,0,0,0];
 
-var ticks = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];$.jqplot('line', [line1, line2, line3], {
+var ticks = [1,2,3,4];$.jqplot('line', [line1, line2, line3], {
         animate: true,
 axesDefaults:{min:0,tickInterval: 1},        seriesDefaults: {
             rendererOptions: {
@@ -18,7 +18,7 @@ axesDefaults:{min:0,tickInterval: 1},        seriesDefaults: {
                 label: "Run Number",
                 ticks: ticks,
                 tickOptions: {
-                    formatString: "%'d "
+                    formatString: "%'d Run"
                 },
                 pad: 1.2,
                 rendererOptions: {
