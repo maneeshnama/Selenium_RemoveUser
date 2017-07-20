ea400a0f1fbf4307c13310d3747876004a9d28be
Maneesh <maneesh@cne-global.com>
2017-07-20 12:08:43 -0500

    new2

--- ATUReports/Results/barChart.js
+++ ATUReports/Results/barChart.js
@@ -1,10 +1,10 @@
             $(document).ready(function(){
-                var s1 = [2,0,4,0,2,0,2,3,9];
-                var s2 = [31,0,29,0,2,0,1,0,0];
-                var s3 = [0,0,0,0,0,0,0,0,0];
+                var s1 = [9,33,2,0,4,0,7,31,8,1,3];
+                var s2 = [0,0,31,0,64,0,26,2,0,0,0];
+                var s3 = [0,0,0,0,0,0,0,0,2,2,0];
 
-var ticks = [1,2,3,4,5,6,7,8,9];    $.jqplot('bar', [s1, s2, s3], {
-        animate: true,axesDefaults:{min:0,tickInterval: 3},        seriesColors: ["#7BB661", "#E03C31", "#21ABCD"],
+var ticks = [9,10,11,12,13,14,15,16,17,18];    $.jqplot('bar', [s1, s2, s3], {
+        animate: true,axesDefaults:{min:0,tickInterval: 6},        seriesColors: ["#7BB661", "#E03C31", "#21ABCD"],
         stackSeries: false,
         seriesDefaults: {
             renderer: $.jqplot.BarRenderer,
--- ATUReports/Results/lineChart.js
+++ ATUReports/Results/lineChart.js
@@ -1,11 +1,11 @@
             $(document).ready(function(){
-                var line1 = [2,0,4,0,2,0,2,3,9];
-                var line2 = [31,0,29,0,2,0,1,0,0];
-                var line3 = [0,0,0,0,0,0,0,0,0];
+                var line1 = [2,0,4,0,2,0,2,3,9,33,2,0,4,0,7,31,8,1,3];
+                var line2 = [31,0,29,0,2,0,1,0,0,0,31,0,64,0,26,2,0,0,0];
+                var line3 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,0];
 
-var ticks = [1,2,3,4,5,6,7,8,9];$.jqplot('line', [line1, line2, line3], {
+var ticks = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18];$.jqplot('line', [line1, line2, line3], {
         animate: true,
-axesDefaults:{min:0,tickInterval: 3},        seriesDefaults: {
+axesDefaults:{min:0,tickInterval: 6},        seriesDefaults: {
             rendererOptions: {
                 smooth: true
             }
@@ -18,7 +18,7 @@ axesDefaults:{min:0,tickInterval: 3},        seriesDefaults: {
                 label: "Run Number",
                 ticks: ticks,
                 tickOptions: {
-                    formatString: "%'d Run"
+                    formatString: "%'d "
                 },
                 pad: 1.2,
                 rendererOptions: {
