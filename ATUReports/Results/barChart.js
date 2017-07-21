c80c4b652ea39a4a11123f3a89b885b003d84f1d
Maneesh <maneesh@cne-global.com>
2017-07-21 16:53:16 -0500

    another 34

--- ATUReports/Results/barChart.js
+++ ATUReports/Results/barChart.js
@@ -1,9 +1,9 @@
             $(document).ready(function(){
-                var s1 = [5,3,2,2,0,2,8,5,10,5];
-                var s2 = [7,10,10,9,0,12,6,5,0,5];
+                var s1 = [3,2,2,0,2,8,5,10,5,25];
+                var s2 = [10,10,9,0,12,6,5,0,5,1];
                 var s3 = [0,0,0,0,0,0,0,0,0,0];
 
-var ticks = [25,26,27,28,29,30,31,32,33,34];    $.jqplot('bar', [s1, s2, s3], {
+var ticks = [26,27,28,29,30,31,32,33,34,35];    $.jqplot('bar', [s1, s2, s3], {
         animate: true,axesDefaults:{min:0,tickInterval: 6},        seriesColors: ["#7BB661", "#E03C31", "#21ABCD"],
         stackSeries: false,
         seriesDefaults: {
