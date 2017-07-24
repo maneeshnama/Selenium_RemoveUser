8be7ee7553b825af2c2f3e7d87172e1db6068366
Maneesh <maneesh@cne-global.com>
2017-07-24 16:07:18 -0500

    new commit 6

--- ATUReports/Results/barChart.js
+++ ATUReports/Results/barChart.js
@@ -1,9 +1,9 @@
             $(document).ready(function(){
-                var s1 = [18,3,3,11,0,9,6,10,12,9];
-                var s2 = [8,15,15,0,0,0,5,1,0,1];
+                var s1 = [3,3,11,0,9,6,10,12,9,3];
+                var s2 = [15,15,0,0,0,5,1,0,1,0];
                 var s3 = [0,0,0,0,0,0,0,0,0,0];
 
-var ticks = [20,21,22,23,24,25,26,27,28,29];    $.jqplot('bar', [s1, s2, s3], {
+var ticks = [21,22,23,24,25,26,27,28,29,30];    $.jqplot('bar', [s1, s2, s3], {
         animate: true,axesDefaults:{min:0,tickInterval: 3},        seriesColors: ["#7BB661", "#E03C31", "#21ABCD"],
         stackSeries: false,
         seriesDefaults: {
